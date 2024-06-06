b116f09ec4f0b0cb669dce6e2de9dfdbd1c5c36f
cecomcrede1 <cecom.crede1@gmail.com>
2024-06-06 08:44:32 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-#import plotly.express as px
+import plotly.express as px
 
 
 # Configura o layout da página para ser "wide" (modo amplo)
